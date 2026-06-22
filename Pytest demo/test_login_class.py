eeb3c24d7efdaea041f5bc93913f6c1e6f96be27
anurags1904 <anurag.singh.ece27@heritageit.edu.in>
2026-06-22 16:05:14 +0530

    pytest

---
+++ Pytest demo/test_login_class.py
@@ -0,0 +1,10 @@
+class TestLoginPage:
+    def test_valid_login(self):
+        assert 'admin' == 'admin'
+
+    def test_invalid_login(self):
+        assert 'admin' != 'guest'
+
+    def test_empty_username(self):
+        username = ''
+        assert username == ''
